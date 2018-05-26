ce399ed8db9d032a8c0e9131b1d77115df4b36fc
qiusz <470542573@qq.com>
2018-05-26 09:30:11 +0800

    update

---
+++ src/ca/main.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const ip = require('ip');
+const fs = require('fs');
+const path = require('path');
+const config = require('../../config.json');
+const myIp = ip.address();
